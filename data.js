e353c53fe5bb990284fdd9b00110776fe2de2aa6
Ananda Sreenidhi <ananda.sreenidhi@gmail.com>
2020-03-02 06:28:02 +0000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,57 @@
+module.exports = function () {
+    return {
+        students: [
+            {
+                id: "1", roll: "17CSU001", name: "Sherlock Holmes", branch: "CSE", gender: "M",
+                dob: "1999-01-01", email: "sherlock17csu001@ncuindia.edu", phone: "9876543210",
+                semester: "IV", cgpa: 10.00, e1: "", e2: "", electiveAllotted: ""
+            },
+            {
+                id: "2", roll: "17MEU002", name: "Lara Croft", branch: "ME", gender: "F",
+                dob: "1999-01-01", email: "lara17csu002@ncuindia.edu", phone: "9876543210",
+                semester: "IV", cgpa: 9.58, e1: "", e2: "", electiveAllotted: ""
+            },
+            {
+                id: "3", roll: "17CSU003", name: "Feluda", branch: "ECE", gender: "M",
+                dob: "1999-05-01", email: "felu17csu003@ncuindia.edu", phone: "9876543210",
+                semester: "IV", cgpa: 10.00, e1: "", e2: "", electiveAllotted: ""
+            },
+            {
+                id: "4", roll: "17CSU004", name: "Byomkesh Bakshy", branch: "Civil", gender: "M",
+                dob: "1999-01-01", email: "byomkesh17csu004@ncuindia.edu", phone: "9876543210",
+                semester: "IV", cgpa: 10.00, e1: "", e2: "", electiveAllotted: ""
+            },
+            {
+                id: "5", roll: "17CSU005", name: "Nancy Drew", branch: "CSE", gender: "F",
+                dob: "1999-01-01", email: "nancy17csu005@ncuindia.edu", phone: "9876543210",
+                semester: "VI", cgpa: 10.00, e1: "", e2: "", electiveAllotted: ""
+            },
+            {
+                id: "6", roll: "17CSU006", name: "L. Auguste Dupin", branch: "CSE", gender: "M",
+                dob: "1999-01-01", email: "auguste17csu006@ncuindia.edu", phone: "9876543210",
+                semester: "VII", cgpa: 10.00, e1: "", e2: "", electiveAllotted: ""
+            },
+            {
+                id: "7", roll: "17CSU007", name: "Bruce Wayne", branch: "ME", gender: "M",
+                dob: "1999-01-01", email: "bruce17csu007@ncuindia.edu", phone: "9876543210",
+                semester: "VI", cgpa: 8.00, e1: "", e2: "", electiveAllotted: ""
+            },
+            {
+                id: "8", roll: "17CSU008", name: "Jane Rizzoli", branch: "CSE", gender: "F",
+                dob: "1999-01-01", email: "jane17csu008@ncuindia.edu", phone: "9876543210",
+                semester: "III", cgpa: 9.20, e1: "", e2: "", electiveAllotted: ""
+            },
+            {
+                id: "9", roll: "17CSU009", name: "Kate Beckett", branch: "Civil", gender: "M",
+                dob: "1999-01-01", email: "kate17csu009@ncuindia.edu", phone: "9876543210",
+                semester: "II", cgpa: 6.00, e1: "", e2: "", electiveAllotted: ""
+            },
+            {
+                id: "10", roll: "17CSU010", name: "L. Lawliet", branch: "CSE", gender: "F",
+                dob: "1999-01-01", email: "lawliet17csu010@ncuindia.edu", phone: "9876543210",
+                semester: "VI", cgpa: 10.00, e1: "", e2: "", electiveAllotted: ""
+            },
+        ],
+        electives: []
+    }
+}
